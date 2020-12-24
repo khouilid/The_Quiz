fd8de6ba5c3b9163984a053fa4114c914d23f27e
khouilid <57531206+khouilid@users.noreply.github.com>
2020-12-24 14:36:55 +0100

    frontend

---
+++ src/Controllers/UserChoice.java
@@ -0,0 +1,29 @@
+package Controllers;
+
+import javafx.scene.Group;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+
+public class UserChoice {
+
+
+    final ToggleGroup redios = new ToggleGroup();
+    public RadioButton userChoice_1;
+    public RadioButton userChoice_2;
+    public RadioButton userChoice_3;
+
+    public ToggleGroup choices;
+
+    public void getUserChoice(MouseEvent mouseEvent) {
+//        userChoice_1.get
+        System.out.println("boom");
+    }
+
+
+
+
+
+
+
+}
--- src/Quiz.java
+++ src/Quiz.java
@@ -1,9 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Quiz extends Application {
@@ -11,35 +9,37 @@ public class Quiz extends Application {
     public void start(Stage stage) throws Exception {
 
 
-        stage.setTitle("The Quiz");
-        stage.setWidth(700);
-        stage.setHeight(700);
+//        stage.setTitle("The Quiz");
+//        stage.setWidth(700);
+//        stage.setHeight(700);
+//
+//
+//        VBox header = new VBox();
+//        header.setId("windows");
+//        header.getStylesheets().add("Stylesheet/Main.css");
+//
+//        Label title = new Label("The Quiz");
+//
+//
+//        Text title2 = new Text("Level: " );
+//
+//        title2.setId("title");
+//        header.getChildren().addAll(title, title2);
+//
+//        Scene scene = new Scene(header);
+//        scene.getStylesheets().add("Stylesheet/Main.css");
+//
+//
+//
+//
+//
+//
+//
+//        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("Public/Quiz.fxml"));
 
+        stage.setScene(new Scene(root));
 
-        VBox header = new VBox();
-        header.setId("windows");
-
-        VBox header2 = new VBox();
-        header2.setId("windows2");
-
-        Label title = new Label("The Quiz");
-        title.setId("title");
-        title.setTextFill(Color.WHITE);
-        title.setFont(new Font("Roboto",26));
-
-        header2.getChildren().addAll(title);
-        header.getChildren().addAll(header2);
-
-        Scene scene = new Scene(header);
-        scene.getStylesheets().add("Stylesheet/Main.css");
-
-
-
-
-
-
-
-        stage.setScene(scene);
         stage.show();
 
     }
