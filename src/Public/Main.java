9a2a4fdf31d39a84cf29efda0fbe00ed9e467937
khouilid <57531206+khouilid@users.noreply.github.com>
2021-01-04 14:52:19 +0100

    convert java file into jar

--- src/Public/Main.java
+++ src/Public/Main.java
@@ -2,18 +2,12 @@ package Public;
 
 import javafx.application.Application;
 
-//import static Controllers.UserChoice.times;
-
 public class Main {
 
 
     public static void main(String[] args) {
 
-
         Application.launch(Quiz.class, args);
-
-//        times();
-
 
     }
 }
